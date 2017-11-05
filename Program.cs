3e08ef346fda02e0a3f402fdb4901ef9561be6d6
RobiiiOlen <32923360+RobiiiOlen@users.noreply.github.com>
2017-11-05 12:58:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersice_HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
